c67ae847859e5c480bec21a812fd42603903ea16
Rakibul-islam231 <ri2311258@gmail.com>
2022-01-31 09:05:53 -0800

    Intern project one

---
+++ assets/js/style.js
@@ -0,0 +1,125 @@
+// movies banner js 
+
+$(".banner-slider").slick({
+  prevArrow: '.left',
+  nextArrow: '.right',
+});
+// movies banner end
+$('.lat').slick({
+    infinite: true,
+    dots:false,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    prevArrow:'.arrow-prev',
+    nextArrow:'.arrow-next',
+
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: false,
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
+  $('.upc').slick({
+    infinite: true,
+    dots:false,
+    speed: 300,
+    
+    prevArrow:'.arrow-prev-upc',
+    nextArrow:'.arrow-next-upc',
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: false,
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
+  $('.sug').slick({
+    infinite: true,
+    dots:false,
+    speed: 300,
+    
+    
+    prevArrow:'.arrow-prev-sug',
+    nextArrow:'.arrow-next-sug',
+    slidesToShow: 4,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: false,
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
+
+  
